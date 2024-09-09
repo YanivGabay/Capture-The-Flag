f206ee5e8e67c106a2ab7c97f08ff7b08985063d
Yaniv Gabay <yanivga@edu.hac.ac.il>
2024-09-09 11:25:36 +0300

    working basic env.py and main

--- level_5_python_best_practices/part1/env.py
+++ level_5_python_best_practices/part1/env.py
@@ -1,1 +1,16 @@
-ÿþ
+import os
+from dotenv import load_dotenv
+import validators
+
+load_dotenv()
+
+ENV = os.getenv('ENV','development')
+print(f"Running in {ENV} environment")
+ENDPOINT = os.getenv('ENDPOINT_URL')
+print(f"This is the endpoint: {ENDPOINT}")
+
+if not validators.url(ENDPOINT):
+    raise ValueError("Invalid URL")
+
+if ENV not in ['dev','development','prod','production']:
+    ENV = 'development'
